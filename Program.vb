07a26e862bb3798d8135cbe68b845a3df13231b8
TolikVorovchenko22IS <124189191+TolikVorovchenko22IS@users.noreply.github.com>
2023-03-20 10:15:02 +0300

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,23 @@
+Imports System
+
+Module HWcycle
+    Sub Main(args As String())
+        Dim a, p, n As Integer
+        Const max As Integer = 2147483647
+        p = 1
+        Console.WriteLine(" Введите количество вводимых чисел ")
+        n = (Console.ReadLine())
+        For i = 1 To n
+            If p < max / a Then
+                Console.WriteLine(" введите число ")
+                a = (Console.ReadLine())
+                p = p * a
+            Else
+                Console.Write(" Память перегружина !!!")
+                Console.WriteLine(a - 1)
+            End If
+        Next
+        Console.WriteLine(" Произведение N введенных Вами чисел равно ")
+        Console.WriteLine(p)
+    End Sub
+End Module
