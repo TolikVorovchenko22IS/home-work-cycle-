b0b753ee45023067efdaa47a7ca5ddcef8a834d4
TolikVorovchenko22IS <124189191+TolikVorovchenko22IS@users.noreply.github.com>
2023-03-20 10:20:13 +0300

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -1,23 +1,26 @@
-Imports System
+п»їImports System
+Imports System.Data
+Imports System.Runtime.InteropServices
 
-Module HWcycle
+
+Module Program
     Sub Main(args As String())
-        Dim a, p, n As Integer
-        Const max As Integer = 2147483647
-        p = 1
-        Console.WriteLine(" Введите количество вводимых чисел ")
-        n = (Console.ReadLine())
-        For i = 1 To n
-            If p < max / a Then
-                Console.WriteLine(" введите число ")
-                a = (Console.ReadLine())
-                p = p * a
-            Else
-                Console.Write(" Память перегружина !!!")
-                Console.WriteLine(a - 1)
-            End If
+        Dim S As String
+
+
+        Console.WriteLine("Г‚ГўГҐГ¤ГЁГІГҐ Г±ГІГ°Г®ГЄГі")
+        S = Console.ReadLine()
+        For i As Integer = 0 To S.Length - 1
+            Console.Write(S.Chars(i))
+            Console.WriteLine(Asc(S.Chars(i)))
+            Select Case S
+                Case 32
+                    Console.WriteLine("ГЏГ°Г®ГЎГҐГ«Г» ")
+                    Console.WriteLine(Asc(S.Chars(i)))
+                Case 65 To 98
+                    Console.Write(" ГЂГ­ГЈГ«ГЁГ©Г±ГЄГЁГ© ГїГ§Г»ГЄ ")
+            End Select
+
         Next
-        Console.WriteLine(" Произведение N введенных Вами чисел равно ")
-        Console.WriteLine(p)
     End Sub
 End Module
